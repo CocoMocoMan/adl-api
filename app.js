24efd80bffe03ff6cdf6c1b3f7545372960ef7ed
oakel <oakel@ucsd.edu>
2020-02-10 21:29:23 -0800

    user db init and CRUD ops

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "adldb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+});
+
+app.use(bodyParser.json());
+
+app.post('/users/create', (req, res) => {
+  const user = req.body;
+  const values = `null,
+                  '${user.username}',
+                  '${user.firstname}',
+                  '${user.lastname}',
+                  '${user.email}',
+                  '',
+                  '', 
+                  '${user.accesstoken}',
+                  true, 
+                  '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', 
+                  '${new Date().toISOString().slice(0, 19).replace('T', ' ')}'`;
+  const query = `INSERT INTO User VALUES (${values})`;
+  con.query(query, (err, result) => {
+    if(err) {
+        console.log(err);
+        res.send('Error');
+    }
+    res.send(result);
+  });
+});
+
+app.get('/users/get', (req, res) => {
+    const query = `SELECT * FROM User WHERE User.UserID = ${req.query.userID} AND User.Active = true`;
+    con.query(query, (err, results) => {
+      if(err) {
+          console.log(err);
+          res.send('Error');
+      }
+      else if (results.length == 0) res.send('User Not Found');
+      else res.send(results[0]);
+    });
+  });
+  
+app.post('/users/update', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    const query = `UPDATE User SET
+                    Username = '${user.username}',
+                    FirstName = '${user.firstname}',
+                    LastName = '${user.lastname}',
+                    Email = '${user.email}',
+                    Biography = '${user.biography}',
+                    Image = '${user.image}', 
+                    ModifiedDate = '${new Date().toISOString().slice(0, 19).replace('T', ' ')}'
+                    WHERE User.UserID = ${user.userID}`;
+    con.query(query, (err, result) => {
+      if(err) {
+          console.log(err);
+          res.send('Error');
+      }
+      res.send(result);
+    });
+  });
+
+app.get('/users/delete', (req, res) => {
+    const query = `UPDATE User SET Active = false WHERE User.UserID = ${req.query.userID}`;
+    con.query(query, (err, result) => {
+      if(err) {
+          console.log(err);
+          res.send('Error');
+      }
+      else res.send(result);
+    });
+  });
+
+
+
+
+
---
+++ db/create-database.sql
@@ -0,0 +1,82 @@
+DROP DATABASE IF EXISTS adldb;
+CREATE DATABASE  IF NOT EXISTS adldb;
+USE adldb;
+CREATE TABLE IF NOT EXISTS User (
+	UserID 	 int PRIMARY KEY auto_increment,
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email         VARCHAR(50),
+    Biography  text,
+    Image        VARCHAR(100),
+    AccessToken  VARCHAR(100),
+    Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime
+);
+CREATE TABLE IF NOT EXISTS Community (
+	CommunityID int PRIMARY KEY auto_increment,
+    Name VARCHAR(50),
+	Description text, 
+	OwnerID int NOT NULL,
+    Image        VARCHAR(100),
+	Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+	FOREIGN KEY(OwnerID) REFERENCES User(UserID) 
+);
+CREATE TABLE IF NOT EXISTS CommunityUser (
+	CommunityUserID int PRIMARY KEY auto_increment,
+    CommunityID int NOT NULL,
+    UserID int NOT NULL,
+    Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+    FOREIGN KEY(CommunityID) REFERENCES Community(CommunityID) ,
+	FOREIGN KEY(UserID) REFERENCES User(UserID) 
+);
+CREATE TABLE IF NOT EXISTS Post (
+	PostID int PRIMARY KEY auto_increment,
+    Title VARCHAR(200),
+    Type ENUM('MEDIA', 'TEXT', 'POLL'),
+	TextBody text,
+    Image        VARCHAR(100),
+	PosterID int NOT NULL,
+	CommunityID int NOT NULL,
+	Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+	FOREIGN KEY(CommunityID) REFERENCES Community(CommunityID) ,
+    FOREIGN KEY(PosterID) REFERENCES User(UserID) 
+);
+CREATE TABLE IF NOT EXISTS PollOption (
+	PollOptionID int PRIMARY KEY auto_increment,
+    body text,
+	PostID int NOT NULL,
+	Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+    FOREIGN KEY(PostID) REFERENCES Post(PostID) 
+);
+CREATE TABLE IF NOT EXISTS PollOptionUser (
+	PollOptionUserID int PRIMARY KEY auto_increment,
+	PollOptionID int NOT NULL,
+    UserID int NOT NULL,
+	Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+    FOREIGN KEY(PollOptionID) REFERENCES PollOption(PollOptionID),
+	FOREIGN KEY(UserID) REFERENCES User(UserID) 
+);
+CREATE TABLE IF NOT EXISTS Upvote (
+	UpvoteID int PRIMARY KEY auto_increment,
+	PostID int NOT NULL,
+    UserID int NOT NULL,
+	Active		   boolean,
+    CreatedDate datetime,
+    ModifiedDate datetime,
+    FOREIGN KEY(PostID) REFERENCES Post(PostID),
+	FOREIGN KEY(UserID) REFERENCES User(UserID) 
+);
+
+
